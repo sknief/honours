697a089b447fd47c96beba812c6ad81dbd6e1f96
Stella(UbunDorian) <s.knief@uqconnect.edu.au>
2021-07-07 22:20:01 +1000

    first stage of R script completed

--- NAR/ODE_models/ODE_V1.r
+++ NAR/ODE_models/ODE_V1.r
@@ -1,4 +1,17 @@
-#proposed section outline
-# 1. read in file, format and potentially clean
-# 2. calculate ODE values
-# 3. save that into MULTIPLE singleton files
+######################################################
+#           R CODE FOR ODE MODELS AAAAAAA            #
+#    Author: SMS Knief       Date: 07/07/21          #
+######################################################
+
+# 1. read in file, format and potentially clean ######
+params <- read.csv("/home/stella/Desktop/ODE_MODEL_OUTPUT1params.csv", header = FALSE, sep = ",", dec = ".")
+#Q: how will that read in work on tinaroo? whats the location or WD?
+colnames(params) <- c("ID", "Aalpha", "Abeta", "Balpha", "Bbeta")
+
+# 2. calculate ODE values ###########################
+
+
+
+
+
+# 3. save that into MULTIPLE singleton files ########
