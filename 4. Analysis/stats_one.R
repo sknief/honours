d2147ab6a53067ee77135395a17e64884ac06885
Stella Knief <50236762+sknief@users.noreply.github.com>
2021-09-10 00:42:14 +1000

    code completed (kinda? weird half product results)

--- 4. Analysis/stats_one.R
+++ 4. Analysis/stats_one.R
@@ -103,6 +103,7 @@ foreach(i=1:2) %:%
 
       #### THE MAGIC 8 BALL ####
 
+
       ## GRAPH 1: Mean Alpha(A) versus time (line plots and violin plots)
       #only points
       graph1base <- ggplot(data = TETRIS, aes(x = Generation, y = AAlpha))
@@ -111,12 +112,17 @@ foreach(i=1:2) %:%
         theme_classic() +
         labs(x = "Generation", y = "Mean Alpha(A)" )
 
+      ggsave(paste0("Mean_AlphaA_jitter_onerun",j, "_", i, ".png"), device = "png")
+
       #violin plots
       baseplot3 <- ggplot(data = BIGPOPA, aes(x = factor(Generation), y = AAlpha))
       baseplot3 +
         geom_violin() +
         theme_classic() +
         labs(x = "Generation", y = "Mean Alpha(A)")
+
+      ggsave(paste0("Mean_AlphaA_violin_onerun",j, "_", i, ".png"), device = "png")
+
 
       #interquartile range and lines (NOTE: i think this only makes sense with more data / on many model runs)
       graph1base +
@@ -125,6 +131,9 @@ foreach(i=1:2) %:%
         geom_smooth(colour = "lavender") +
         theme_classic() +
         labs(x = "Generation", y = "Mean Alpha(A)")
+
+      ggsave(paste0("Mean_AlphaA_IQR_onerun",j, "_", i, ".png"), device = "png")
+
 
       ## GRAPH 2: Mean Beta(A) versus time (line plots and violin plots)
 
@@ -135,12 +144,16 @@ foreach(i=1:2) %:%
         theme_classic() +
         labs(x = "Generation", y = "Mean Beta(A)")
 
+      ggsave(paste0("Mean_BetaA_jitter_onerun",j, "_", i, ".png"), device = "png")
+
       #violin plots
       violinplot2 <- ggplot(data = BIGPOPA, aes(x = factor(Generation), y = ABeta))
       violinplot2 +
         geom_violin() +
         theme_classic() +
         labs(x = "Generation", y = "Mean Beta(A)")
+
+      ggsave(paste0("Mean_BetaA_violin_onerun",j, "_", i, ".png"), device = "png")
 
       #interquartile range and lines (NOTE: i think this only makes sense with more data / on many model runs)
       graph2base +
@@ -150,6 +163,7 @@ foreach(i=1:2) %:%
         theme_classic() +
         labs(x = "Generation", y = "Mean Beta(A)")
 
+      ggsave(paste0("Mean_BetaA_IQR_onerun",j, "_", i, ".png"), device = "png")
 
       ## GRAPH 3: Mean Alpha(B) versus time (line plots and violin plots)
 
@@ -160,12 +174,16 @@ foreach(i=1:2) %:%
         theme_classic() +
         labs(x = "Generation", y = "Mean Alpha(B)" )
 
+      ggsave(paste0("Mean_AlphaB_jitter_onerun",j, "_", i), ".png", device = "png")
+
       #violin plots
       violinplot3 <- ggplot(data = BIGPOPA, aes(x = factor(Generation), y = BAlpha))
       violinplot3 +
         geom_violin() +
         theme_classic() +
         labs(x = "Generation", y = "Mean Alpha(B)")
+
+      ggsave(paste0("Mean_AlphaB_violin_onerun",j, "_", i, ".png"), device = "png")
 
       #interquartile range and lines (NOTE: i think this only makes sense with more data / on many model runs)
       graph3base +
@@ -174,6 +192,8 @@ foreach(i=1:2) %:%
         geom_smooth(colour = "lavender") +
         theme_classic() +
         labs(x = "Generation", y = "Mean Alpha(B)")
+
+      ggsave(paste0("Mean_AlphaB_IQR_onerun",j, "_", i, ".png"), device = "png")
 
       ## GRAPH 4: Mean Beta(B) versus time (line plots and violin plots)
 
@@ -184,12 +204,16 @@ foreach(i=1:2) %:%
         theme_classic() +
         labs(x = "Generation", y = "Mean Beta(B)")
 
+      ggsave(paste0("Mean_BetaB_jitter_onerun",j, "_", i, ".png"), device = "png")
+
       #violin plots
       violinplot4 <- ggplot(data = BIGPOPA, aes(x = factor(Generation), y = BBeta))
       violinplot4 +
         geom_violin() +
         theme_classic() +
         labs(x = "Generation", y = "Mean Beta(B)")
+
+      ggsave(paste0("Mean_BetaB_violin_onerun",j, "_", i, ".png"), device = "png")
 
       #interquartile range and lines (NOTE: i think this only makes sense with more data / on many model runs)
       graph4base +
@@ -199,6 +223,7 @@ foreach(i=1:2) %:%
         theme_classic() +
         labs(x = "Generation", y = "Mean Beta(A)")
 
+      ggsave(paste0("Mean_BetaB_IQR_onerun",j, "_", i, ".png"), device = "png")
 
       ## GRAPH 5: Mean Integral of A versus time (line plots and violin plots)
 
@@ -209,12 +234,20 @@ foreach(i=1:2) %:%
         theme_classic() +
         labs(x = "Generation", y = "Mean ACONC")
 
+      ggsave(paste0("AConc_jitter_onerun",j, "_", i, ".png"), device = "png")
+
+
+      #---stops working after here for some reason ----?
+
+
       #violin plots
       violinplot5 <- ggplot(data = BIGPOPA, aes(x = factor(Generation), y = AConc))
       violinplot5 +
         geom_violin() +
         theme_classic() +
         labs(x = "Generation", y = "Mean ACONC")
+
+      ggsave(paste0("AConc_violin_onerun",j, "_", i, ".png"), device = "png")
 
       #interquartile range and lines (NOTE: i think this only makes sense with more data / on many model runs)
       graph5base +
@@ -223,6 +256,8 @@ foreach(i=1:2) %:%
         geom_smooth(colour = "lavender") +
         theme_classic() +
         labs(x = "Generation", y = "Mean ACONC")
+
+      ggsave(paste0("AConc_IQR_onerun",j, "_", i, ".png"), device = "png")
 
       ## GRAPH 6: Mean Integral of B versus time (line plots and violin plots)
 
@@ -233,12 +268,16 @@ foreach(i=1:2) %:%
         theme_classic() +
         labs(x = "Generation", y = "Mean BCONC")
 
+      ggsave(paste0("BConc_jitter_onerun",j, "_", i, ".png"), device = "png")
+
       #violin plots
       violinplot6 <- ggplot(data = BIGPOPA, aes(x = factor(Generation), y = BConc))
       violinplot6 +
         geom_violin() +
         theme_classic() +
         labs(x = "Generation", y = "Mean BCONC")
+
+      ggsave(paste0("BConc_violin_onerun",j, "_", i, ".png"), device = "png")
 
       #interquartile range and lines (NOTE: i think this only makes sense with more data / on many model runs)
       graph6base +
@@ -247,6 +286,8 @@ foreach(i=1:2) %:%
         geom_smooth(colour = "lavender") +
         theme_classic() +
         labs(x = "Generation", y = "Mean ACONC")
+
+      ggsave(paste0("BConc_IQR_onerun",j, "_", i, ".png"), device = "png")
 
       ## GRAPH 7: Mean Population Fitness versus time (line plots and violin plots)
       # this one is a two part deal:
@@ -267,12 +308,16 @@ foreach(i=1:2) %:%
         theme_classic() +
         labs(x = "Generation", y = "Mean Fitness")
 
+      ggsave(paste0("Fitness_jitter_onerun",j, "_", i, ".png"), device = "png")
+
       #violin plots
       violinplot7 <- ggplot(data = BIGPOPA, aes(x = factor(Generation), y = fitness))
       violinplot7 +
         geom_violin() +
         theme_classic() +
         labs(x = "Generation", y = "Mean Fitness")
+
+      ggsave(paste0("Fitness_violin_onerun",j, "_", i, ".png"), device = "png")
 
       #interquartile range and lines (NOTE: i think this only makes sense with more data / on many model runs)
       graph7base +
@@ -281,6 +326,8 @@ foreach(i=1:2) %:%
         geom_smooth(colour = "lavender") +
         theme_classic() +
         labs(x = "Generation", y = "Mean ACONC")
+
+      ggsave(paste0("Fitness_IQR_onerun",j, "_", i, ".png"), device = "png")
 
 
       ## GRAPH 8: ODE samples from individual runs @ different time points
@@ -292,7 +339,7 @@ foreach(i=1:2) %:%
 
       BIGPOPA$distance <- BIGPOPA$BConc-BOpt
 
-
+      #density plot
       distancebase <- ggplot(BIGPOPA, aes(x = distance, colour = factor(BIGPOPA$Generation)))
       distancebase +
         geom_density() +
@@ -301,10 +348,16 @@ foreach(i=1:2) %:%
         xlim(-41, -39) +
         theme(legend.position = "bottom")
 
+      ggsave(paste0("Distance_density_onerun",j, "_", i, ".png"), device = "png")
+
+      #mean density?
       TETRIS$distance <- BIGPOPA %>%
         group_by(BIGPOPA$Generation, .add = FALSE) %>%
         summarise(distance = mean(distance)) %>%
         pull(distance)
+
+
+      ###potential interfering for save data
 
       distancebase2 <- ggplot(TETRIS, aes(x =Generation, y = distance, colour = factor(TETRIS$Generation)))
       distancebase2 +
@@ -314,34 +367,30 @@ foreach(i=1:2) %:%
         ylim(-41, -39) +
         theme(legend.position = "none")
 
-      #SAVE CODE FOR THE DATASETS
+      ggsave(paste0("Fitness_jitter_onerun",j, "_", i, ".png"), device = "png")
+
+
+#SAVE CODE
+  #for the datasets
+      #Tetris
+      teddy <- as.character(paste0("Tetris_onerun_", j, "_", i, ".csv"))
+
+      Tetris <- as.data.frame(TETRIS)
+
+      write.table(Tetris, teddy,
+                  append = FALSE,
+                  row.names = FALSE,
+                  col.names = TRUE)
+
+      #BigPopa
+      BigPop <- as.character(paste0("BigPopa_onerun_", j, "_", i, ".csv"))
+
+      write.table(BIGPOPA, BigPop,
+                  append = FALSE,
+                  row.names = FALSE,
+                  col.names = TRUE)
+
+  #for the graphs is under the actual graph code
+
 
 } #foreach closing bracket
-
-$$$$$$$$$$$$$$$$$$ CODE GRAVEYARD $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-#raw data: BConc
-baseplot <- ggplot(data = BIGPOPA, aes(x = factor(Generation), y = BConc))
-baseplot + geom_violin()
-
-baseplot2 <- ggplot(data = BIGPOPA, aes(x = Generation, y = BConc))
-baseplot2 + geom_point(position = "jitter")
-
-#raw data: AConc
-baseplot3 <- ggplot(data = BIGPOPA, aes(x = factor(Generation), y = AConc))
-baseplot3 + geom_violin()
-
-baseplot4 <- ggplot(data = BIGPOPA, aes(x = Generation, y = AConc))
-baseplot4 + geom_point(position = "jitter")
-
-
-
-
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  ###left to do
-  # automation
-  # SEED key (test ADD as well when you're at it)
-  # save code for the graphs
-  # unique save code for the data sets so that they can feed into larger analyses
-  # rebuy a hardrive cord
-
-  #test
