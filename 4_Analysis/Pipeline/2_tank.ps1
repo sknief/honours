c4f808c7f40319baddbc2a338f964c7f648e44cc
Stella Knief <50236762+sknief@users.noreply.github.com>
2021-09-19 19:17:58 +1000

    made the ps1 script to move things!

--- 4_Analysis/Pipeline/2_tank.ps1
+++ 4_Analysis/Pipeline/2_tank.ps1
@@ -1,7 +1,70 @@
-#for test
-cd C:\Users\sknie\github\honours\4. Analysis\Test_files\ODE\BOptHigh\pbs.530888[1].tinmgr2
+#for test files, location needs to be updated later on
+
+#ODE
+### BOptHigh,
+# go in and make tank
+cd C:\Users\sknie\github\honours\4_Analysis\ODE\BOptHigh
+mkdir tank
+
+Get-ChildItem Tetris*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ODE\BOptHigh -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ODE\BOptHigh\tank
+Get-ChildItem BigPopa*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ODE\BOptHigh -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ODE\BOptHigh\tank
+
+### BOptMed,
+#go in and make tank
+cd C:\Users\sknie\github\honours\4_Analysis\ODE\BOptmed
+mkdir tank
+
+Get-ChildItem Tetris*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ODE\BOptMed -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ODE\BOptMed\tank
+Get-ChildItem BigPopa*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ODE\BOptMed -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ODE\BOptMed\tank
+
+### BOptLow,
+# go in and make tank
+cd C:\Users\sknie\github\honours\4_Analysis\ODE\BOptLow
+mkdir tank
+
+Get-ChildItem Tetris*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ODE\BOptLow -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ODE\BOptLow\tank
+Get-ChildItem BigPopa*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ODE\BOptLow -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ODE\BOptLow\tank
+
+### Neutral,
+#go in and make tank
+cd C:\Users\sknie\github\honours\4_Analysis\ODE\Neutral
+mkdir tank
+
+Get-ChildItem Tetris*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ODE\Neutral -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ODE\Neutral\tank
+Get-ChildItem BigPopa*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ODE\Neutral -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ODE\Neutral\tank
 
 
-MKDIR
+#ADD
+### BOptHigh,
+# go in and make tank
+cd C:\Users\sknie\github\honours\4_Analysis\ADD\BOptHigh
+mkdir tank
 
-MOVE
+Get-ChildItem Tetris*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ADD\BOptHigh -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ADD\BOptHigh\tank
+Get-ChildItem BigPopa*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ADD\BOptHigh -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ADD\BOptHigh\tank
+
+### BOptMed,
+#go in and make tank
+cd C:\Users\sknie\github\honours\4_Analysis\ADD\BOptMed
+mkdir tank
+
+Get-ChildItem Tetris*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ADD\BOptMed -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ADD\BOptMed\tank
+Get-ChildItem BigPopa*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ADD\BOptMed -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ADD\BOptMed\tank
+
+
+### BOptLow,
+# go in and make tank
+cd C:\Users\sknie\github\honours\4_Analysis\ADD\BOptLow
+mkdir tank
+
+Get-ChildItem Tetris*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ADD\BOptLow -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ADD\BOptLow\tank
+Get-ChildItem BigPopa*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ADD\BOptLow -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ADD\BOptLow\tank
+
+
+### Neutral
+#go in and make tank
+cd C:\Users\sknie\github\honours\4_Analysis\ADD\Neutral
+mkdir tank
+
+Get-ChildItem Tetris*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ADD\Neutral -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ADD\Neutral\tank
+Get-ChildItem BigPopa*.csv -Path C:\Users\sknie\github\honours\4_Analysis\ADD\Neutral -Recurse | Move-Item -Destination C:\Users\sknie\github\honours\4_Analysis\ADD\Neutral\tank
--- 4_Analysis/Pipeline/4_aquarium.ps1
+++ 4_Analysis/Pipeline/4_aquarium.ps1
@@ -1,1 +1,18 @@
-move data from tank folder to aquarium folder
+#for test files, location needs to be updated later on
+
+# go in and make aquarium
+cd C:\Users\sknie\github\honours\4_Analysis\
+mkdir aquarium
+
+#files come from the tanks:
+## ODE
+Move-Item -Path C:\Users\sknie\github\honours\4_Analysis\ODE\BOptMed\tank\Pog*.csv -Destination C:\Users\sknie\github\honours\4_Analysis\ODE\aquarium
+Move-Item -Path C:\Users\sknie\github\honours\4_Analysis\ODE\BOptLow\tank\Pog*.csv  -Destination C:\Users\sknie\github\honours\4_Analysis\ODE\aquarium
+Move-Item -Path C:\Users\sknie\github\honours\4_Analysis\ODE\BOptHigh\tank\Pog*.csv  -Destination C:\Users\sknie\github\honours\4_Analysis\ODE\aquarium
+Move-Item -Path C:\Users\sknie\github\honours\4_Analysis\ODE\Neutral\tank\Pog*.csv  -Destination C:\Users\sknie\github\honours\4_Analysis\ODE\aquarium
+
+## ADD
+Move-Item -Path C:\Users\sknie\github\honours\4_Analysis\ADD\BOptMed\tank\Pog*.csv -Destination C:\Users\sknie\github\honours\4_Analysis\ADD\aquarium
+Move-Item -Path C:\Users\sknie\github\honours\4_Analysis\ADD\BOptLow\tank\Pog*.csv  -Destination C:\Users\sknie\github\honours\4_Analysis\ADD\aquarium
+Move-Item -Path C:\Users\sknie\github\honours\4_Analysis\ADD\BOptHigh\tank\Pog*.csv  -Destination C:\Users\sknie\github\honours\4_Analysis\ADD\aquarium
+Move-Item -Path C:\Users\sknie\github\honours\4_Analysis\ADD\Neutral\tank\Pog*.csv  -Destination C:\Users\sknie\github\honours\4_Analysis\ADD\aquarium
