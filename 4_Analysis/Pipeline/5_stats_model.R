4f17caf30ca691b6af6596da0c67dc19bb7c762d
Stella Knief <50236762+sknief@users.noreply.github.com>
2021-09-19 20:45:21 +1000

    added a second loop for speed sake

--- 4_Analysis/Pipeline/5_stats_model.R
+++ 4_Analysis/Pipeline/5_stats_model.R
@@ -5,6 +5,12 @@
 ##       CITE WHAT YOU USE, KO-FI IF YOU'RE NICE      ##
 ########################################################
 
+#### Amamteur Parallelisation ####
+library(dplyr)
+library(ggplot2)
+library(foreach)
+library(gridExtra)
+
 #######################################
 #user input here!
 MODELTYPE <- "ODE"
@@ -13,14 +19,9 @@ MODELTYPE <- "ODE"
 #objective of this part: take in all the param combos and average across those, right? can alwas check tmrw
 #sample across the entire parameter space to see how these values evolve? if they reach optima
 
-#### Amamteur Parallelisation ####
-library(dplyr)
-library(ggplot2)
-library(foreach)
-library(gridExtra)
 
 ####  set WD to aquarium #####
-workdirectory <- paste0("C:/Users/sknie/github/honours/4. Analysis/Test_files/", MODELTYPE, "/aquarium")
+workdirectory <- paste0("C:/Users/sknie/github/honours/4_Analysis/", MODELTYPE, "/aquarium")
 setwd(workdirectory)
 
 ## THE FILES LOOP ##
@@ -129,6 +130,114 @@ foreach(m=1:4) %do% {
     
     #Elodea, nope
     
+}
+    
+
+# STOP 
+
+#HIT THE BREAKS
+
+# THE BIG LOOP FT PLOTS: 
+
+foreach(m=1:4) %do% {
+  
+  ##### the two loops to set up the params for the rest of the code #####
+  
+  #set the OPTIMA, then load the files
+  if (m == 1) {
+    OPTIMA <- "BOptHigh"
+  } else if (m == 2) {
+    OPTIMA <- "BOptMed"
+  } else if (m == 3) {
+    OPTIMA <- "BOptLow"
+  } else if (m == 4) {
+    OPTIMA <- "Neutral"
+  } else {
+    print("What the dog doin")
+  }
+  
+  #Second nested loop: setting BOpt depending on your input and model type
+  if (MODELTYPE == "ADD") {
+    if (OPTIMA == "BOptHigh") {
+      BOpt = 200
+    } else if (OPTIMA == "BOptMed") {
+      BOpt = 150
+    } else if (OPTIMA == "BOptLow") {
+      BOpt = 100
+    } else if (OPTIMA == "Neutral") {
+      BOpt = 0
+    } else {
+      print("Invalid Input for the Optima. Would you like to try again?")
+    }
+  } else if (MODELTYPE == "ODE") {
+    if (OPTIMA == "BOptHigh") {
+      BOpt = 100
+    } else if (OPTIMA == "BOptMed") {
+      BOpt = 50
+    } else if (OPTIMA == "BOptLow") {
+      BOpt = 10
+    } else if (OPTIMA == "Neutral") {
+      BOpt = 0
+    } else {
+      print("Invalid Input for the Optima. Would you like to try again?")
+    }
+  } else {
+    print("I do not recognize that model type, did you spell something wrong?")
+  }
+  
+  #### Read in the files ####
+  #this gives me all seeds (*) for a given combination of node and index
+  myElodea <- lapply(Sys.glob(paste0("PogNoodle_*_", OPTIMA, "*.csv")), read.table) #reps for unique combos, seed level data
+  myShelldon <- lapply(Sys.glob(paste0("PogNoodled_*_", OPTIMA, "*.csv")), read.table) #one entry per generation, mean across seeds
+  
+  #Elodea : the dataset with ALL seeds for all combos, might be too big for the large datasets
+  ELODEA <- bind_rows(myElodea, .id = "UniqueCombo") #all params comb, one entry per generation
+  colnames(ELODEA) <- ELODEA[1,] #column names
+  colnames(ELODEA)[1] <- "UniqueCombo" #fix one label
+  ELODEA <- subset(ELODEA, AAlpha!= "AAlpha") #remove the labels
+  ELODEA <-   mutate_all(ELODEA, .funs = as.numeric) #turns characters into numerics
+  
+  #Sheldon : the dataset with the means for all combos
+  SHELLDON <- bind_rows(myShelldon, .id = "UniqueCombo") #all params comb, one entry per generation
+  colnames(SHELLDON) <- SHELLDON[1,] #column names
+  colnames(SHELLDON)[1] <- "UniqueCombo" #fix one label
+  SHELLDON <- subset(SHELLDON, AAlpha!= "AAlpha") #remove the labels
+  SHELLDON <-   mutate_all(SHELLDON, .funs = as.numeric) #turns characters into numerics
+  
+  #take the means across replicates (seeds) of the means across individuals (but via generation? yeah, cause each time point is unique data, and if i only want the end i still need to do it by generation)
+  RICHARD <- SHELLDON %>%
+    group_by(SHELLDON$Generation) %>%
+    summarise(AAlpha = mean(AAlpha),
+              ABeta = mean(ABeta),
+              BAlpha = mean(BAlpha),
+              BBeta = mean(BBeta),
+              AConc = mean(AConc),
+              BConc = mean(BConc),
+              fitness = mean(fitness),
+              distance = mean(distance)
+    )
+  
+  colnames(RICHARD)[1] <- "Generation"
+  
+  #Richard
+  richie <- as.character(paste0("Richard_", MODELTYPE, "_", OPTIMA, ".csv"))
+  
+  Richard <- as.data.frame(RICHARD)
+  
+  write.table(Richard, richie,
+              append = FALSE,
+              row.names = FALSE,
+              col.names = TRUE)
+  
+  #Shelldon
+  shellie <- as.character(paste0("Shelldon_", MODELTYPE, "_", OPTIMA, ".csv"))
+  
+  write.table(SHELLDON, shellie,
+              append = FALSE,
+              row.names = FALSE,
+              col.names = TRUE)
+  
+
     #### make the plots ####
     #violin plots will make more sense than line plots i reckon
     
@@ -398,49 +507,49 @@ foreach(m=1:4) %do% {
     g1 <-      
       graph1base +
       geom_point(position = "jitter", color = "orchid2") +
-      geom_line(color = "skyblue1") +
+      geom_line(data = RICHARD, color = "skyblue1") +
       theme_classic() +
       labs(x = "Generation", y = "Mean Alpha(A)" )
     g2 <- 
       graph2base +
       geom_point(position = "jitter", color = "orchid2") +
-      geom_line(color = "skyblue1") +
+      geom_line(data = RICHARD, color = "skyblue1") +
       theme_classic() +
       labs(x = "Generation", y = "Mean Beta(A)")
     g3 <-       
       graph3base +
       geom_point(position = "jitter", color = "orchid2") +
-      geom_line(color = "skyblue1") +
+      geom_line(data = RICHARD, color = "skyblue1") +
       theme_classic() +
       labs(x = "Generation", y = "Mean Alpha(B)" )
     g4 <- 
       graph4base +
       geom_point(position = "jitter", color = "orchid2") +
-      geom_line(color = "skyblue1") +
+      geom_line(data = RICHARD, color = "skyblue1") +
       theme_classic() +
       labs(x = "Generation", y = "Mean Beta(B)")
     g5 <- 
       graph5base +
       geom_point(position = "jitter", color = "orchid2") +
-      geom_line(color = "skyblue1") +
+      geom_line(data = RICHARD, color = "skyblue1") +
       theme_classic() +
       labs(x = "Generation", y = "Mean ACONC")
     g6 <- 
       graph6base +
       geom_point(position = "jitter", color = "orchid2") +
-      geom_line(color = "skyblue1") +
+      geom_line(data = RICHARD, color = "skyblue1") +
       theme_classic() +
       labs(x = "Generation", y = "Mean BCONC")
     g7 <-      
       graph7base +
       geom_point(position = "jitter", color = "orchid2") +
-      geom_line(color = "skyblue1") +
+      geom_line(data = RICHARD, color = "skyblue1") +
       theme_classic() +
       labs(x = "Generation", y = "Mean Fitness")
     g8 <- 
-      distancebase2 +
+      graph8base +
       geom_point(color = "orchid2") +
-      geom_line(color = "skyblue1") +
+      geom_line(data = RICHARD, color = "skyblue1") +
       theme_classic() +
       labs(x = "Generations", y = "Distance to the optima") +
       ylim(-41, -39) +
@@ -468,7 +577,7 @@ foreach(m=1:4) %do% {
       guides(fill = "none") +
       labs(x = "Generation", y = "Mean BConc" )
       
-    #ggsave(paste0("Adaptive Walk_", MODELTYPE, "_", OPTIMA,".png"), device = "png")
+    ggsave(paste0("Adaptive Walk_", MODELTYPE, "_", OPTIMA,".png"), device = "png")
     
     
 } #foreach loop ends here 
