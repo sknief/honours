a8ff0851fdd5a9de75a19299100bf6bd199efad6
Stella Knief <50236762+sknief@users.noreply.github.com>
2021-10-23 22:53:43 +1000

    fixed typo

--- 4_Analysis/Pipeline/HPC_Pipe/stats_one.R hauls/SUBLAUNCHER3.R
+++ 4_Analysis/Pipeline/HPC_Pipe/stats_one.R hauls/SUBLAUNCHER3.R
@@ -20,7 +20,7 @@ combos <- read.csv(paste0("/home/",USER,"/Analysis/numbers.csv"), header = T)
 
 # Set which runs to do according to node
 
-switch (ARR_INDEX,
+switch (ARRAY_INDEX,
         { combos <- combos[1,] },
         { combos <- combos[2,] },
         { combos <- combos[3,] },
