e0400663475f285b59587896c1f18bc6d912f83d
Teteris <s.knief@uqconnect.edu.au>
2021-11-02 21:32:51 +1000

    secdon batch

---
+++ 3_HPC/OutbackRuns/ODE/BOptLow/Data Collection/SUBLAUNCHERMINI3.R
@@ -0,0 +1,46 @@
+##############################################################################################################
+#  Run SLiM in parallel
+##############################################################################################################
+
+#  Parallel script modified from SLiM-Extras example R script, info at
+#  the SLiM-Extras repository at https://github.com/MesserLab/SLiM-Extras.
+
+# Environment variables
+
+USER <- Sys.getenv('USER')
+ARRAY_INDEX <- as.numeric(Sys.getenv('PBS_ARRAY_INDEX'))
+
+# Parallelisation libraries
+
+library(foreach)
+library(doParallel)
+library(future)
+
+
+seeds <- read.csv(paste0("/home/",USER,"/OutbackRuns/ODE/nanoseeds.csv"), header = T)
+combos <- read.csv(paste0("/home/",USER,"/OutbackRuns/ODE/minicombo3.csv"), header = T)
+
+# Set which runs to do according to node
+
+switch (ARRAY_INDEX,
+        { combos <- combos[1,] },
+        { combos <- combos[2,] },
+        { combos <- combos[3,] },
+        { combos <- combos[4,] }
+)
+
+
+
+cl <- makeCluster(future::availableCores())
+registerDoParallel(cl)
+
+#Run SLiM
+foreach(i=1:nrow(combos)) %:%
+  foreach(j=seeds$Number) %dopar% {
+    # Use string manipulation functions to configure the command line args, feeding from a data frame of seeds
+    # then run SLiM with system(),
+    slim_out <- system(sprintf("/usr/bin/slim -s %s -d GeneA1=%f -d GeneA2=%f -d GeneB1=%f -d GeneB2=%f -d modelindex=%i ~/OutbackRuns/ODE/BOptLow/Model.slim",
+                               as.character(j), combos[i,]$Aalpha, combos[i,]$Abeta, combos[i,]$Balpha, combos[i,]$Bbeta, i, intern=T))
+  }
+
+stopCluster(cl)
